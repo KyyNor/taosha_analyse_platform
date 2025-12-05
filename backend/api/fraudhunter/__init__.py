e0efc79dc2846c8b4e6ddfc8fa58cc8c91d4d889
kyynor <kyynor@my.com>
2025-12-05 18:43:11 +0800

    编写ds对接计划

--- backend/api/fraudhunter/__init__.py
+++ backend/api/fraudhunter/__init__.py
@@ -4,7 +4,7 @@ FraudHunter API路由模块
 
 from .indicator_task_routes import router as indicator_task_router
 from .indicator_routes import router as indicator_router
-from .task_routes import router as task_router
+from .dry_run_task_routes import router as task_router
 
 __all__ = [
     'indicator_task_router',
