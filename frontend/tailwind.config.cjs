6d419fbe6adff67f1717b7ab5888bb7d23a1492e
kyynor <kyynor@my.com>
2025-12-08 19:14:41 +0800

    解决背景透明的问题

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -35,21 +35,24 @@ module.exports = {
   				DEFAULT: 'hsl(var(--destructive))',
   				foreground: 'hsl(var(--destructive-foreground))'
   			},
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		},
+  			                        card: {
+  			                                DEFAULT: 'hsl(var(--card))',
+  			                                foreground: 'hsl(var(--card-foreground))'
+  			                        },
+  			                        popover: {
+  			                          DEFAULT: 'hsl(var(--popover))',
+  			                          foreground: 'hsl(var(--popover-foreground))',
+  			                        },
+  			                        sidebar: {
+  			                                DEFAULT: 'hsl(var(--sidebar-background))',
+  			                                foreground: 'hsl(var(--sidebar-foreground))',
+  			                                primary: 'hsl(var(--sidebar-primary))',
+  			                                'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  			                                accent: 'hsl(var(--sidebar-accent))',
+  			                                'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  			                                border: 'hsl(var(--sidebar-border))',
+  			                                ring: 'hsl(var(--sidebar-ring))'
+  			                        }  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
