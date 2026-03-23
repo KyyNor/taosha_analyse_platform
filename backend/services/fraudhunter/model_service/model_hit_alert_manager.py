4438d4035c4bdb0912c8781145c84b1a0b6e7d61
whwangzeqi <whwangzeqi@hxb.com.cn>
2026-03-23 17:06:19 +0800

    应要求改为只当天不重复预警，后续还是会预警

--- backend/services/fraudhunter/model_service/model_hit_alert_manager.py
+++ backend/services/fraudhunter/model_service/model_hit_alert_manager.py
@@ -318,7 +318,7 @@ class ModelHitAlertManager:
         """
         # 查询当天是否已有管控记录
         from datetime import timedelta
-        date_range = [date - timedelta(days=i) for i in range(3)]  # 今天、昨天、前天
+        date_range = [date - timedelta(days=i) for i in range(1)]  # 今天 , 应要求改为只当天不重复预警，后续还是会预警
 
         existing_control = self.db.query(FraudHunterModelAlertControlRecord).filter(
             FraudHunterModelAlertControlRecord.account_id == account_id,
--- sbackend.sh
+++ sbackend.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "Starting backend server..."
-cd backend
-uv run uvicorn main:app --workers=2 --port 50011
+cd /data/taosha/taosha_analyse_platform
+source .venv/bin/activate
+cd /data/taosha/taosha_analyse_platform/backend
+rm -rf .worker_registry/
+nohup uvicorn main:app --workers=4 --port 50011 --host 0.0.0.0 &
+tail -200f nohup.out
